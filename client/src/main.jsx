73e5a38184d7f345306970066fffebf8885fcacb
iampriyanshu29 <impriyanshu2901@gmail.com>
2024-02-03 10:07:40 +0530

    add redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,48 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Signin from './pages/Signin.jsx'
-import Signup from './pages/Signup.jsx'
-import DashBoard from './pages/Dashboard.jsx'
-import About from './pages/About.jsx'
-import { Provider } from 'react-redux'
-import {store} from './redux/store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Signin from "./pages/Signin.jsx";
+import Signup from "./pages/Signup.jsx";
+import DashBoard from "./pages/Dashboard.jsx";
+import About from "./pages/About.jsx";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    children:[
-              {
-                path:"/",
-                element:<Home/>
-              },
-              {
-                path:"/signin",
-                element:<Signin/>
-              },
-              {
-                path:"/signup",
-                element:<Signup/>
-              },
-              {
-                path:"/dashboard",
-                element:<DashBoard/>
-              },
-              {
-                path:"/about",
-                element:<About/>
-              }
-    ]
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashBoard />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>,
-)
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
